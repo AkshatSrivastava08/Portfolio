8e42d89444d953a576233acb55198a55f681a63c
Akshat <srivastavaakshat080901@gmail.com>
2025-06-09 23:52:27 +0530

    first commit

---
+++ client/src/components/ContactSection.jsx
@@ -0,0 +1,192 @@
+import { Mail, MapPin, Phone, Send } from "lucide-react";
+import { FaInstagram } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+import { FaXTwitter } from "react-icons/fa6";
+import { SlSocialLinkedin } from "react-icons/sl";
+import { cn } from "@/lib/utils";
+import { useToast } from "@/hooks/use-toast";
+import { useState } from "react";
+
+export const ContactSection = () => {
+  const { toast } = useToast();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    const res = await fetch("http://localhost:5000/send", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: e.target.name.value,
+        email: e.target.email.value,
+        message: e.target.message.value,
+      }),
+    });
+
+    const result = await res.json();
+
+    if (res.ok) {
+      toast({ title: "Message Sent!", description: result.message });
+    } else {
+      toast({ title: "Failed to send", description: result.message });
+    }
+
+    setIsSubmitting(false);
+  };
+
+  return (
+    <section id="contact" className="py-24 px-4 relative bg-secondary/30">
+      <div className="container mx-auto max-x-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          Get In <span className="text-primary">Touch</span>
+        </h2>
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          have a project in mind or want to collaborate? Feel free to reach out.
+          I am always open to discussing new opportunities.
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+          <div className="space-y-8">
+            <h3 className="text-2xl font-semibold mb-6">Contact Information</h3>
+            <div className="space-y-6 justify-center">
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Mail className="h-6 w-6 text-primary" />
+                </div>
+                <div>
+                  <h4 className="font-medium">Email</h4>
+                  <a
+                    href="mailto:srivastavaakshat080901@gmail.com"
+                    className="text-muted-foreground hover:text-primary transition-colors"
+                  >
+                    srivastavaakshat080901@gmail.com
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Phone className="h-6 w-6 text-primary" />
+                </div>
+                <div>
+                  <h4 className="font-medium">Phone</h4>
+                  <a
+                    href="tel:+919140999018"
+                    className="text-muted-foreground hover:text-primary transition-colors"
+                  >
+                    +919140999018
+                  </a>
+                </div>
+              </div>
+              <div className="flex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <MapPin className="h-6 w-6 text-primary" />
+                </div>
+                <div>
+                  <h4 className="font-medium">Location</h4>
+                  <a className="text-muted-foreground hover:text-primary transition-colors">
+                    Lucknow, India
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="pt-8">
+              <h4 className="font-medium mb-4"> Connect With Me</h4>
+              <div className="flex space-x-4 justify-center">
+                <a
+                  href="https://www.linkedin.com/in/akshatsrivastava2001/"
+                  target="_blank"
+                >
+                  <SlSocialLinkedin className="h-6 w-6" />
+                </a>
+                <a href="https://x.com/AkshatS080901" target="_blank">
+                  <FaXTwitter className="h-6 w-6" />
+                </a>
+                <a href="https://www.instagram.com/akshat7961/" target="_blank">
+                  <FaInstagram className="h-6 w-6" />
+                </a>
+                <a href="https://leetcode.com/u/Akshat809/" target="_blank">
+                  <SiLeetcode className="h-6 w-6" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className="bg-card p-8 rounded-lg shadow-xs"
+            onSubmit={handleSubmit}
+          >
+            <h3 className="text-2xl font-semibold mb-6"> Contact Me</h3>
+
+            <form className="space-y-6">
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  required
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
+                  placeholder="John Doe..."
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  required
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
+                  placeholder="john@gmail.com"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-medium mb-2"
+                >
+                  {" "}
+                  Your Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  required
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary resize-none"
+                  placeholder="Hello, I'd like to talk about..."
+                />
+              </div>
+
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className={cn(
+                  "cosmic-button w-full flex items-center justify-center gap-2"
+                )}
+              >
+                {isSubmitting ? "Sending..." : "Send Message"}
+                <Send size={16} />
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ client/src/components/ProjectsSection.jsx
@@ -0,0 +1,142 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react";
+
+const projects = [
+  {
+    id: 1,
+    title: "myCryptoTracker",
+    description:
+      "A Cryptocurrency Tracking App include Top 10 cryptocurrencies, Trending coins, and a customizable Watchlist.",
+    image: "public/projects/project1.png",
+    tags: ["React", "TailwindCSS", "Redux Toolkit"],
+    demoUrl: "https://mycryptotrackerr.netlify.app/",
+    githubUrl: "https://github.com/AkshatSrivastava08/myCryptoTracker",
+  },
+  {
+    id: 2,
+    title: "HomeScout",
+    description:
+      "A real estate web app for discovering and listing properties with map integration and real-time chat system.",
+    image: "public/projects/project2.png",
+    tags: ["React", "Leaflet.js", "socket.io", "SCSS"],
+    demoUrl: "https://github.com/AkshatSrivastava08/Real_estate_webapp",
+    githubUrl: "https://github.com/AkshatSrivastava08/Real_estate_webapp",
+  },
+  {
+    id: 3,
+    title: "DevCraftr",
+    description:
+      "Code Editor for HTML, CSS, and JavaScript with live preview functionality for real-time editing and rendering.",
+    image: "public/projects/project3.png",
+    tags: ["React", "CSS", "codemirror library"],
+    demoUrl: "https://devcraftr.netlify.app/",
+    githubUrl: "https://github.com/AkshatSrivastava08/DevCraftr",
+  },
+  {
+    id: 4,
+    title: "The Book Burrow",
+    description:
+      "A Web platform to search for various books using google books api.",
+    image: "public/projects/project4.png",
+    tags: ["React", "CSS", "google books api"],
+    demoUrl: "https://thebookburrow.netlify.app/",
+    githubUrl: "https://github.com/AkshatSrivastava08/Book_Library_app",
+  },
+  {
+    id: 5,
+    title: "Gourmet Gems",
+    description:
+      "A Web Application to search for various Food Recipes using TheMealDB api.",
+    image: "public/projects/project5.png",
+    tags: ["React", "Node.js", "TheMealDB api"],
+    demoUrl: "https://gourmetgems.netlify.app/",
+    githubUrl: "https://github.com/AkshatSrivastava08/Recipe_app",
+  },
+  {
+    id: 6,
+    title: "Pomodor app",
+    description:
+      "A pomodoro web app with 25 mins work timer and 5 mins break timer to increase focus.",
+    image: "public/projects/project6.png",
+    tags: ["React", "Node.js"],
+    demoUrl: "https://pomodorotimerwebapp.netlify.app/",
+    githubUrl: "https://github.com/AkshatSrivastava08/POMODORO_app",
+  },
+];
+
+export const ProjectsSection = () => {
+  return (
+    <section id="projects" className="py-24 px-4 relative">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          {" "}
+          Featured <span className="text-primary"> Projects </span>
+        </h2>
+
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Here are some of my recent projects. Each project was carefully
+          crafted with attention to detail, performance, and user experience.
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((project, key) => (
+            <div
+              key={key}
+              className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+            >
+              <div className="h-48 overflow-hidden">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                />
+              </div>
+
+              <div className="p-6">
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {project.tags.map((tag) => (
+                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+
+                <h3 className="text-xl font-semibold mb-1"> {project.title}</h3>
+                <p className="text-muted-foreground text-sm mb-4">
+                  {project.description}
+                </p>
+                <div className="flex justify-between items-center">
+                  <div className="flex space-x-3">
+                    <a
+                      href={project.demoUrl}
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    >
+                      <ExternalLink size={20} />
+                    </a>
+                    <a
+                      href={project.githubUrl}
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    >
+                      <Github size={20} />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="text-center mt-12">
+          <a
+            className="cosmic-button w-fit flex items-center mx-auto gap-2"
+            target="_blank"
+            href="https://github.com/AkshatSrivastava08"
+          >
+            Check My Github <ArrowRight size={16} />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
